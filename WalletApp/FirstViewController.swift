68def3cc3e8003a7856a13e595729497b1d1b2f0
Andras Bohacsek <iboha@icloud.com>
2019-08-23 22:23:02 +0200

    Initial Commit

---
+++ WalletApp/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  WalletApp
+//
+//  Created by Andras Bohacsek on 2019. 08. 23..
+//  Copyright © 2019. Andras Bohacsek. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
