6a2e690f4d11e9a9bad96c003cd8d1da226c9e07
Andras Bohacsek <iboha@icloud.com>
2019-08-28 20:40:34 +0200

    add ResultViewController

---
+++ WalletApp/Controllers/ResultViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ResultViewController.swift
+//  WalletApp
+//
+//  Created by Andras Bohacsek on 2019. 08. 28..
+//  Copyright © 2019. Andras Bohacsek. All rights reserved.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
